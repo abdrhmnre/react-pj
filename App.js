e8065afe16b64b9d48f258d13c04087550b22103
abdrhmnre <129596425+abdrhmnre@users.noreply.github.com>
2023-11-16 12:41:35 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+  function handelNext() {
+    if (step < 3) setStep((s) => s + 1);
+    console.log(step);
+  }
+  function handelPrevious() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+  return (
+    <>
+      <button
+        className="close"
+        onClick={() => {
+          setIsOpen(!isOpen);
+          console.log(isOpen);
+          console.log(step);
+        }}
+      >
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""} `}>2</div>
+            <div className={`${step >= 3 ? "active" : ""} `}>3</div>
+          </div>
+          <p className="message">
+            step {step}:{messages[step - 1]}{" "}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handelPrevious}
+            >
+              previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handelNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
